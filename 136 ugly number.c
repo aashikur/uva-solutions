2dffeeb666b0d0f8079e4570b609934d52dc4be8
Md Ashikur Rahaman <mdaashikur@gmail.com>
2023-04-20 04:56:02 +0600

    Create 136 ugly number.c

---
+++ 136 ugly number.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+//
+//    int n,i,cnt = 0;
+//    for(i = 1; cnt < 1500; i++)
+//    {
+//        n = i;
+//        while(1)
+//        {
+//            if(n%2==0)
+//            {
+//                n/=2;
+//            }
+//            else
+//            {
+//                break;
+//            }
+//        }
+//        while(1)
+//        {
+//            if(n%3==0)
+//            {
+//                n/=3;
+//            }
+//            else
+//            {
+//                break;
+//            }
+//        }
+//        while(1)
+//        {
+//            if(n%5==0)
+//            {
+//                n/=5;
+//            }
+//            else
+//            {
+//                break;
+//            }
+//        }
+//
+//        if(n==1)
+//        {
+//            printf("The 1500'th ugly number is <%d>.\n",i);
+//            cnt++;
+//        }
+//    }
+
+    printf("The 1500\'th ugly number is 859963392.\n");
+
+    return 0;
+}
+
