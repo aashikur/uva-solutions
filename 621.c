e0868d72f081f5d554992465a9bc6a99b000bc88
Md Ashikur Rahaman <mdaashikur@gmail.com>
2023-04-19 12:51:42 +0600

    Not Proper Rule

---
+++ 621.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    unsigned long long int num,r;
+    int x,a,t;
+    char ans;
+
+    scanf("%d",&t);
+    while(t--)
+    {
+        scanf("%llu",&num);
+
+        x = (int)log10(num) +1;
+        a = num / (int)pow(10,x-2);
+
+        if(num == 4 || num == 1 || num == 78) ans = '+';
+        else if(num%100 == 78) ans = '?';
+        else if( a == 78 ) ans = '-';
+        else ans = '*';
+
+        printf("%c\n",ans);
+
+    }
+
+
+    return 0;
+}
