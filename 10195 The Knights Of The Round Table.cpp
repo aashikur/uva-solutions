f1b3c2e7f9737df9ce708f22186c4dd894189b6b
Md Ashikur Rahaman <mdaashikur@gmail.com>
2023-04-23 23:22:17 +0600

    Update 10195 The Knights Of The Round Table.cpp

--- 10195 The Knights Of The Round Table.cpp
+++ 10195 The Knights Of The Round Table.cpp
@@ -9,17 +9,19 @@ int main(void)
 {
 
     double a,b,c,s, result;
-    cin >> a>>b>>c;
+    while(cin >> a>>b>>c)
+    {
+        s = (a+b+c)/2.0;
 
-    s = (a+b+c)/2.0;
+        if(a == 0 || b == 0 || c== 0)
+            result = 0;
+        else if(a + b < c || b + c< a || a+c <b)
+            result = 0;
+        else
+            result = sqrt(((s-a)*(s-b)*(s-c))/s);
 
-    if(a == 0 || b == 0 || c== 0)
-        result = 0;
-    else
-        result = sqrt(((s-a)*(s-b)*(s-c))/s);
-
-    printf("The radius of the round table is: %.3lf\n",result);
-
+        printf("The radius of the round table is: %.3lf\n",result);
+    }
 
     return 0;
 }
