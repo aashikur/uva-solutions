2ab86b8864918160032c28cc2399eb4fa3c777f9
Md Ashikur Rahaman <mdaashikur@gmail.com>
2023-04-24 01:08:19 +0600

    Create 10970 Big Chocolate.cpp

---
+++ 10970 Big Chocolate.cpp
@@ -0,0 +1,16 @@
+//* Bismillahir Rahmanir Raham *//
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int a, b, res;
+    while(cin >> a>>b)
+    {
+        res = b*a -1;
+        printf("%d\n",res);
+    }
+
+    return 0;
+}
