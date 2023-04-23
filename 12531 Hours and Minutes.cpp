80e490077b077ded8df4c52af60f0ec870d3939f
Md Ashikur Rahaman <mdaashikur@gmail.com>
2023-04-23 21:36:33 +0600

    Create 12531 Hours and Minutes.cpp

---
+++ 12531 Hours and Minutes.cpp
@@ -0,0 +1,19 @@
+//** Bismillahir Rahmanir Rohim**//
+
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+int angle;
+while(scanf("%d",&angle) != EOF)
+{
+    if(angle%6 == 0)
+        printf("Y\n");
+    else
+        printf("N\n");
+}
+
+    return 0;
+}
