e4ce1d6bcbd148e2b844121f665ce063438d017b
Md Ashikur Rahaman <mdaashikur@gmail.com>
2023-08-12 00:40:10 +0600

    Create 10469 To Carry or not to Carry.c

---
+++ 10469 To Carry or not to Carry.c
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b, c;
+
+    while (scanf("%d %d",&a,&b) != EOF)
+    {
+        c = a ^ b;
+
+        cout << c << endl;
+    }
+
+}
